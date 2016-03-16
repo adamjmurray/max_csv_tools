811804b9e10f6f1a451d8c40a333f706720dcd99
Adam Murray <adamjmurray@gmail.com>
2016-03-15 18:56:23 -0700

    automatically resize cellblock to handle the tsv input

--- csv2cellblock.js
+++ csv2cellblock.js
@@ -1,22 +1,28 @@
 function read(filepath) {
-	var file = new File(filepath, 'read', 'TEXT');
-	if (!file.isopen) {
-		error('Could not open file: ' + filepath + '\n');
-		return;
-	}
-	var text = '';
-	var eof = file.eof;
-	var i, j;
-	for(i = 0; i < eof; i+= 1024) {
-		text += file.readchars(1024).join('');	
-	}
-	var lines = text.split('\n');
-	for(i = 0; i < lines.length; i++) {
-		// TODO: support CSV
-		// TODO: handle escaped tabs
-		var cells = lines[i].split('\t');
-		for(j = 0; j < cells.length; j++) {
-			outlet(0, ['set', j, i, cells[j]]);
-		}
-	}
+  var file = new File(filepath, 'read', 'TEXT');
+  if (!file.isopen) {
+    error('Could not open file: ' + filepath + '\n');
+    return;
+  }
+  var text = '';
+  var eof = file.eof;
+  var i, j;
+  for(i = 0; i < eof; i += 1024) {
+    text += file.readchars(1024).join('');
+  }
+  var lines = text.split('\n');
+  var cols = 0;
+  outlet(0, 'rows', lines.length);
+  for(i = 0; i < lines.length; i++) {
+    // TODO: support CSV
+    // TODO: handle escaped tabs
+    var cells = lines[i].split('\t');
+    if (cells.length > cols) {
+      cols = cells.length;
+      outlet(0, 'cols', cols);
+    }
+    for(j = 0; j < cells.length; j++) {
+      outlet(0, 'set', j, i, cells[j]);
+    }
+  }
 }
